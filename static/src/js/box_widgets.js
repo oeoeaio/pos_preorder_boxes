31ca3eaf528aaa997b98e2309ce43b2ca1797282
Rob Harrington <oeoeaio@gmail.com>
2018-04-30 21:16:36 +1000

    Use precalculated box counts to compute widget content

--- static/src/js/box_widgets.js
+++ static/src/js/box_widgets.js
@@ -7,7 +7,6 @@ var BoxWidget = chrome.StatusWidget.extend({
     start: function(){
         var self = this;
 
-        self.find_product_ids();
         self.count_boxes();
 
         self.pos.bind('preorders:updated',self.count_boxes,this);
@@ -24,53 +23,38 @@ var BoxWidget = chrome.StatusWidget.extend({
             this.$('.js_count').html(0);
         }
     },
-    find_product_ids: function(){
-        var all_ids = Object.keys(this.pos.db.product_by_id);
-        var ids = [];
-        for (var i=0;i<all_ids.length;i++){
-            var product = this.pos.db.product_by_id[all_ids[i]];
-            if (product.display_name.startsWith(this.size + ' Box')){
-                ids.push(parseInt(all_ids[i]));
-            }
-        }
-        this.product_ids = ids;
-    },
     count_boxes: function(){
         var count = 0;
         var preorders = this.pos.db.get_preorders_sorted();
+        var count_property = this.size+'_count';
         for (var i=0;i<preorders.length;i++){
             var preorder = preorders[i];
             if (preorder.state == 'to_deliver') continue;
-            for (var j=0;j<preorder.lines.length;j++){
-              var line = preorder.lines[j];
-                if (this.product_ids.indexOf(line.product_id[0]) > -1){
-                    count += line.qty;
-                }
-            }
+            count += preorder[count_property];
         }
         this.set_count(count);
     },
 });
 
 var SmallBoxWidget = BoxWidget.extend({
-    template: 'SmallBoxWidget',
+    template: 'BoxWidget',
     init: function(parent, options) {
         this._super(parent, options);
-        this.size = "Small";
+        this.size = "small";
     },
 });
 var StandardBoxWidget = BoxWidget.extend({
-    template: 'StandardBoxWidget',
+    template: 'BoxWidget',
     init: function(parent, options) {
         this._super(parent, options);
-        this.size = "Standard";
+        this.size = "standard";
     },
 });
 var LargeBoxWidget = BoxWidget.extend({
-    template: 'LargeBoxWidget',
+    template: 'BoxWidget',
     init: function(parent, options) {
         this._super(parent, options);
-        this.size = "Large";
+        this.size = "large";
     },
 });
 
