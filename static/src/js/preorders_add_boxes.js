601b7c733a73881b4a4538e33674f1957b54b1b7
Rob H <oeoeaio@gmail.com>
2020-05-22 23:09:12 +1000

    V13 upgrade: use new load_fields method to append fields to pos.preorder model

--- static/src/js/preorders_add_boxes.js
+++ static/src/js/preorders_add_boxes.js
@@ -1,19 +1,15 @@
-odoo.define('pos.preorders.add_boxes', function (require) {
+odoo.define('pos.preorders.add_boxes', ['point_of_sale.models', 'pos.preorders'], function (require) {
 
 var models = require('point_of_sale.models');
 
-// Find the model request for preorders
-// and add box size fields to the field list
-var model_list = models.PosModel.prototype.models;
-for (var i=0;i < model_list.length;i++){
-    if (model_list[i].model === 'pos.preorder'){
-        model_list[i].fields.push('box_size');
-        model_list[i].fields.push('small_count');
-        model_list[i].fields.push('standard_count');
-        model_list[i].fields.push('large_count');
-        model_list[i].fields.push('fruit_count');
-        model_list[i].fields.push('large_fruit_count');
-    }
-}
+// Add box size fields to the field list for preorders
+models.load_fields('pos.preorder', [
+  'box_size',
+  'small_count',
+  'standard_count',
+  'large_count',
+  'fruit_count',
+  'large_fruit_count',
+])
 
 });
